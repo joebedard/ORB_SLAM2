f9a8a13a8b67514a4f0df63d45608dbd7c670c01
Joe Bedard <mr.joe.bedard@gmail.com>
2019-04-05 20:52:39 +0200

    implement resize_images

--- tools/resize_images.cc
+++ tools/resize_images.cc
@@ -1,20 +1,106 @@
 #include <time.h>
 #include <iostream>
+#include <string>
+#include <filesystem>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace experimental;
 
+double gScalingRatio;
+string gSourcePathStr;
+string gDestinPathStr;
+filesystem::path gSourcePath;
+filesystem::path gDestinPath;
 
-int main(int argc, char **argv) {
+bool ParseParams(int argc, char * argv[])
+{
+   if (argc != 4)
+   {
+      cout << "Usage: ./resize_images scaling_ratio source_path destination_path";
+      return false;
+   }
 
-  if (argc != 3)
-  {
-     cout << "Usage: ./resize_images scaling_ratio source_path destination_path";
-     return -1;
-  }
+   gScalingRatio = atof(argv[1]);
+   if (0.0 >= gScalingRatio || 1.0 <= gScalingRatio)
+   {
+      cout << "scaling_ratio should be between 0.0 and 1.0";
+      return false;
+   }
 
-  double scalingRatio;
+   gSourcePathStr = string(argv[2]);
+   filesystem::path sourcePath(gSourcePathStr);
+   if (!filesystem::exists(sourcePath))
+   {
+      cout << "source_path does not exist";
+      return false;
+   }
+   gSourcePath = filesystem::path(gSourcePathStr);
 
-  cout << "Resize Images with scaling ratio " << scalingRatio << endl;
+   gDestinPathStr = string(argv[3]);
+   filesystem::path destinPath(gSourcePathStr);
+   if (!filesystem::exists(destinPath))
+   {
+      cout << "destination_path does not exist";
+      return false;
+   }
+   gDestinPath = filesystem::path(gDestinPathStr);
 
-  return 0;
+   return true;
 }
+
+int main(int argc, char **argv) try
+{
+   if (ParseParams(argc, argv))
+   {
+      cout << "Resize Images with scaling ratio " << gScalingRatio << endl;
+      cout << "Source Path: " << gSourcePathStr << endl;
+      cout << "Destination Path: " << gDestinPathStr << endl;
+
+      vector<string> sourceFiles, destinFiles;
+      for (auto & begit : filesystem::directory_iterator(gSourcePath))
+      {
+         filesystem::path p = begit.path();
+         if (p.has_extension())
+         {
+            if (0 == p.extension().compare(string(".png")))
+            {
+               sourceFiles.push_back(p.string());
+               destinFiles.push_back(gDestinPathStr + "\\" + p.filename().string());
+            }
+         }
+      }
+
+      for (int i = 0; i < sourceFiles.size(); ++i)
+      {
+         cv::Mat sourceImg = cv::imread(sourceFiles[i]);
+         cv::Mat destinImg;
+         cv::resize(sourceImg, destinImg, cv::Size(0,0), gScalingRatio, gScalingRatio, CV_INTER_AREA);
+         cout << destinFiles[i] << endl;
+         cv::imwrite(destinFiles[i], destinImg);
+      }
+
+      return EXIT_SUCCESS;
+   }
+   else
+      return EXIT_FAILURE;
+}
+catch( cv::Exception & e ) 
+{
+   string msg = string("cv::Exception: ") + e.what();
+   cerr << "main: " << msg << endl;
+   return EXIT_FAILURE;
+}
+catch (const exception & e)
+{
+   string msg = string("exception: ") + e.what();
+   cerr << "main: " << msg << endl;
+   return EXIT_FAILURE;
+}
+catch (...)
+{
+   string msg = string("There was an unknown exception in the main thread.");
+   cerr << "main: " << msg << endl;
+   return EXIT_FAILURE;
+}
+
